f0945ee8282570ff831c587ede994a393ee5a663
Achref <medachrefg@gmail.com>
2022-09-07 14:34:36 +0100

    working on the frequently ask question section

--- scripts/app.js
+++ scripts/app.js
@@ -3,3 +3,18 @@ const nav = document.querySelector('nav');
 window.addEventListener('scroll', () => {
 	nav.classList.toggle('window-scroll', window.scrollY > 0);
 });
+
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach((faq) => {
+	faq.addEventListener('click', () => {
+		faq.classList.toggle('open');
+
+		const icon = faq.querySelector('.faq__icon i');
+		if (icon.className === 'fa-solid fa-plus') {
+			icon.className = 'fa-solid fa-minus';
+		} else {
+			icon.className = 'fa-solid fa-plus';
+		}
+	});
+});
