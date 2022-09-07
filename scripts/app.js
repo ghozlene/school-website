2c0a730bee5b79fb6b51e36f30dfc612dccbbf26
Achref <medachrefg@gmail.com>
2022-09-07 18:24:24 +0100

    working on testimonial section

--- scripts/app.js
+++ scripts/app.js
@@ -18,3 +18,23 @@ faqs.forEach((faq) => {
 		}
 	});
 });
+var swiper = new Swiper('.mySwiper', {
+	slidesPerView: 2,
+	spaceBetween: 30,
+	slidesPerGroup: 2,
+	loop: true,
+	loopFillGroupWithBlank: true,
+
+	autoplay: {
+		delay: 2500,
+		disableOnInteraction: false,
+	},
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+	},
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+});
